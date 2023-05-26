3a5599425b4afaae8fd64fe31298336306dded7e
bociwess0 <dimitrijeveljkovic2@gmail.com>
2023-05-26 23:06:22 +0200

    Header and initial changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './components/header/header.module';
+import { HomePageModule } from './components/home-page/home-page.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    HomePageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
